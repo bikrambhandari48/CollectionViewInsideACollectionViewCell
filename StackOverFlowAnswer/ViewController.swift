487e0215634ac1451918a0ad4ccebd9c60984ebd
BIKRAM BHANDARI <BIKRAM@>
2017-06-18 22:20:27 +1000

    Initial Commit

---
+++ StackOverFlowAnswer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackOverFlowAnswer
+//
+//  Created by BIKRAM BHANDARI on 18/6/17.
+//  Copyright © 2017 BIKRAM BHANDARI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
